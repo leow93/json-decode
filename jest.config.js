67a463ef839b4c4e459c021daa11452232e55943
leow93 <leowheelan@hotmail.co.uk>
2023-10-07 16:17:02 +0100

    chore: migrate from jest to vitest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  coverageProvider: 'v8',
-  transform: {
-    '^.+\\.(t|j)sx?$': ['@swc/jest'],
-  },
-  testMatch: ['<rootDir>/src/**/*.spec.{ts,js}'],
-};
